7ef33a1a36a4cbe4a8d6535125fa714beaf2c22e
Deepa <58700597+mabaedk75@users.noreply.github.com>
2020-08-26 17:16:09 +0530

    Update mini.php
    28_08_2020Lab

--- mini.php
+++ mini.php
@@ -61,6 +61,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $gender = test_input($_POST["gender"]);
   }
+
+  if (empty($fnameErr) && empty($lnameErr) && empty($agErr) && empty($emailErr) && empty($genderErr) && empty($unameErr)) {
+	header("Location: home.php");
+}
 }
 
 function test_input($data) {
@@ -69,6 +73,7 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -77,6 +82,54 @@ function test_input($data) {
 	<meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="minime.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Patrick Hand">
+	<script>
+
+		function validateForm(){
+
+			var names = /^[A-Za-z]+$/;
+			var all = /^[a-zA-Z0-9@_]+$/;
+
+			var fname = document.getElementById("fname").value;
+			var lname = document.getElementById("lname").value;
+			var uname = document.getElementById("uname").value;
+			var age = document.signform.age.value;
+			var email = document.signform.email.value;
+			var atposition=email.indexOf("@");
+			var dotposition=email.lastIndexOf(".");
+
+			if (!names.test(fname)){  
+			alert("Enter a valid first name");  
+			  return false;  
+			}
+
+			else if(!names.test(lname)){  
+			  alert("Enter a valid last name");  
+			  return false;  
+			}
+
+			else if(!all.test(uname) || uname.value.length<6){
+				alert("Invalid Username");
+				return false;
+			}
+
+			else if(atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length){
+			alert("Please enter a valid e-mail address");
+			return false;
+			}
+
+			else if(isNaN(age)){
+				document.getElementById("numloc").innerHTML="Enter numeric input only";
+				return false;
+			}
+
+			else{
+				return true;
+			}
+
+
+
+		}
+	</script>
 	</head>
 <body>
 	<div class="parallax">
@@ -84,20 +137,20 @@ function test_input($data) {
 		<div class="container">
 		<center><div class="h1">Sign Up</div></center>
 		<p><span class="error">* required field</span></p>
-			<form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-				<input type="text" name="fname" placeholder="First Name" value="<?php echo $fname ?>">
+			<form name ="signform" onsubmit="return validateForm()" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+				<input type="text" id = "fname" name="fname" placeholder="First Name" value="<?php echo $fname ?>">
 				<span class="error">*<?php echo $fnameErr ?></span><br/>
-				<input type="text" name="lname" placeholder="Last Name" value="<?php echo $lname ?>">
+				<input type="text" id = "lname" name="lname" placeholder="Last Name" value="<?php echo $lname ?>">
 				<span class="error">*<?php echo $lnameErr ?></span><br/>
-				<input type="text" name="uname" placeholder="Username(atleast 6 charachters)" value="<?php echo $uname ?>">
+				<input type="text" id="uname" name="uname" placeholder="Username(atleast 6 charachters)" value="<?php echo $uname ?>">
 				<span class="error">*<?php echo $unameErr ?></span><br/>
-				<input type="text" name="age" maxlength="3" placeholder="Age" value="<?php echo $age ?>">
+				<input type="text" name="age" maxlength="3" placeholder="Age" value="<?php echo $age ?>"><span id="numloc"></span>
 				<span class="error">*<?php echo $ageErr ?></span><br/>
 				<input type="email" name="email" placeholder="Email" value="<?php echo $email ?>">
 				<?php if(isset($emailErr)) { ?>
 					<span class="error">*<?php echo $emailErr ?></span>
 				<?php } ?><br/>
-				<input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female"><font size="3px">Female</font>
+				<input type="radio" name="gender" id="" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female"><font size="3px">Female</font>
 				<input type="radio" name="gender" <?php if (isset($gender) && $gender == 'male') echo 'checked'; ?> value="male"><font size="3px">Male</font>
 				<input type="radio" name="gender" <?php if (isset($gender) && $gender == 'other') echo 'checked'; ?> value="other"><font size="3px">Other</font>
 				<span class="error">* <?php echo $genderErr;?></span><br/><br/>
